f0510634e463de858d57dd315d65507ed1fd9105
klok <klok@mk1>
2018-12-04 14:47:56 -0200

    Conexao UDP e TCP

--- Medidor.java
+++ Medidor.java
@@ -1,53 +1,139 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * 
+ * UNIVERSIDADE DO VALE DO ITAJAÍ
+ * Redes de Computadores 1
+ * 
+ * Uso de TCP / UDP / HTTP
+ * 
+ * @author Gabriel Hegler Klok
+ * @author Maykon Anschau Oliveira
+ * 
  */
-package kansocket;
-
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 import java.net.Socket;
+import java.util.Random;
+import java.util.Scanner;
 
-/**
- *
- * @author klok
- */
 public class Medidor {
 
     public static void main(String[] args){
         if(args.length != 3){
-            System.err.println("Usage: java Medidor <ip address> <port> <message>");
+            System.err.println("Usage: java kansocket.Medidor <ip of server TCP> <port connection TCP> <port server UDP>");
             System.exit(-1);
         }
         
+        String ipOfServer = args[0];
+        int portConnection = Integer.parseInt(args[1]);
+        int portServer = Integer.parseInt(args[2]);
+            
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                startServerUDP(portServer);
+            }
+        }).start();
+                   
+        interval(2000);      
+        
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                startClientTCP(ipOfServer, portConnection);
+            }
+        }).start();
+    }
+    
+    private static void startServerUDP(int port){
+        boolean exit = false;
+        String message;
+        DatagramSocket socket;
+        DatagramPacket datagram;
+        
         try {
             
-            String ipServer = args[0];
-            int portServer = Integer.parseInt(args[1]);
-            String message = args[2];
+            socket = new DatagramSocket(port);
+            System.out.println("Listening to port " + String.valueOf(port));
             
-            Socket socket = new Socket(ipServer, portServer);
-            
-            DataOutputStream dataOutput = new DataOutputStream(socket.getOutputStream());
-            dataOutput.writeUTF(message);
-            
-            DataInputStream dataInput = new DataInputStream(socket.getInputStream());
-            String data = dataInput.readUTF();
+            do {                
+                datagram = new DatagramPacket(new byte[1024], 1024);
+                socket.receive(datagram);
+                
+                message = new String(datagram.getData()).trim();
+                
+                if (message.equalsIgnoreCase("exit")) {
+                    exit = true;
+                }
+                
+                System.out.println("Received " + message + " from " +
+                        datagram.getAddress() + ":" + datagram.getPort());
+                
+            } while (!exit);
             
             socket.close();
             
-            if (data.equals(message)) {
-                System.out.println("Successfull");
-            }else{
-                System.out.println("send: " + message);
-                System.out.println("Received: " + data);
-            }
+        } catch (Exception e) {
+            System.err.println("An exception ocourred: "+e.getMessage());
+            e.printStackTrace();
+            System.exit(-1);
+        } 
+    }
+    
+    private static void startClientTCP(String server, int port){
+        Scanner scanner = new Scanner(System.in);
+        String message, data = "";
+        boolean stop = false;
+        DataOutputStream dataOutput;
+        DataInputStream dataInput;
+        Random random = new Random();
+        int consumo = 0;
+        
+        try {
             
-        } catch (Exception e) {  
+            Socket socket;
+            
+            do {
+                System.out.print("Aguardando próxima aferição");
+                for(int i = 0; i < 5; i++){
+                    System.out.print(".");
+                    Thread.sleep(1000);
+                }
+                consumo += random.nextInt(20);
+                message = String.valueOf( consumo );     
+                
+                System.out.println("Consumo atual enviado: " + message);
+                
+                socket = new Socket(server, port);
+
+                dataOutput = new DataOutputStream(socket.getOutputStream());
+                dataOutput.writeUTF(message);
+
+                dataInput = new DataInputStream(socket.getInputStream());
+                data = dataInput.readUTF();
+
+                socket.close();
+
+                if(!data.equalsIgnoreCase(message)){
+                    System.out.println("Message is not valid!");
+                }
+                
+            } while (!stop);
+            
+        } catch (Exception e) {
             System.err.println("Erro: " + e.getMessage());
             e.printStackTrace();
             System.exit(-1);
         }
+    }
+    
+    private static void interval(long value){
+        try {
+            Thread.sleep(value);
+        } catch (Exception e) {
+            System.err.println("Error in sleep: "+e.getMessage());
+            System.exit(-1);
+        } 
     }
 }
--- SmartGateway.java
+++ SmartGateway.java
@@ -1,43 +1,71 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * 
+ * UNIVERSIDADE DO VALE DO ITAJAÍ
+ * Redes de Computadores 1
+ * 
+ * Uso de TCP / UDP / HTTP
+ * 
+ * @author Gabriel Hegler Klok
+ * @author Maykon Anschau Oliveira
+ * 
  */
-package kansocket;
-
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-/**
- *
- * @author klok
- */
 public class SmartGateway {
     
-    public static void main(String[] args){
-        
-        if(args.length != 1){
-            System.err.println("Usage: java SmartGateway <port>");
+    public static void main(String[] args){     
+        if(args.length != 3){
+            System.err.println("Usage: java kansocket.SmartGateway <ip of server UDP> <port connection UDP> <port server TCP>");
             System.exit(-1);
         }
+
+        String ipOfServer = args[0];
+        int portConnection = Integer.parseInt(args[1]);
+        int portServer = Integer.parseInt(args[2]);
         
-        try {
-            int port = Integer.parseInt(args[0]);
-            boolean exit = false;
-            
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                startServerTCP(portServer);
+            }
+        }).start();
+        
+        interval(2000);
+        
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                startClientUDP(ipOfServer, portConnection, "oieeeee");
+            }
+        }).start();
+    }
+    
+    private static void startServerTCP(int port){
+        boolean exit = false;
+        DataInputStream dataInput;
+        DataOutputStream dataOutput;
+        String data;
+        
+        try {         
+        
             ServerSocket serverSocket = new ServerSocket(port);
-            
-            do {                
-                
+            System.out.println("Listening to port " + String.valueOf(port));
+
+            do {            
+
                 Socket socket = serverSocket.accept();
-                
+
                 System.out.println("Received message from " +
                         socket.getInetAddress().getHostName() + ": " + socket.getPort());
                 
-                DataInputStream dataInput = new DataInputStream(socket.getInputStream());
-                String data = dataInput.readUTF();
+                dataInput = new DataInputStream(socket.getInputStream());
+                data = dataInput.readUTF();
                 
                 if(data.equalsIgnoreCase("exit")){
                     exit = true;
@@ -45,15 +73,15 @@ public class SmartGateway {
                 
                 System.out.println("Echoing " + data);
                 
-                DataOutputStream dataOutput = new DataOutputStream(socket.getOutputStream());
+                dataOutput = new DataOutputStream(socket.getOutputStream());
                 dataOutput.writeUTF(data);
                 
                 socket.close();
-                
+
             } while (!exit);
             
             serverSocket.close();
-            
+
         } catch (Exception e) {
             System.err.println("Erro: " + e.getMessage());
             e.printStackTrace();
@@ -62,4 +90,28 @@ public class SmartGateway {
     }
     
     
+    private static void startClientUDP(String ip, int port, String message){        
+        try {            
+            DatagramSocket socket = new DatagramSocket();
+
+            DatagramPacket datagram = new DatagramPacket(message.getBytes(), 0, message.getBytes().length, InetAddress.getByName(ip), port);
+
+            socket.send(datagram);
+            socket.close();
+            
+        } catch (Exception e) {
+            System.err.println("An exception ocourred: "+e.getMessage());
+            e.printStackTrace();
+            System.exit(-1);
+        }
+    }
+    
+    private static void interval(long value){
+        try {
+            Thread.sleep(value);
+        } catch (Exception e) {
+            System.err.println("Error in sleep: "+e.getMessage());
+            System.exit(-1);
+        } 
+    }
 }
